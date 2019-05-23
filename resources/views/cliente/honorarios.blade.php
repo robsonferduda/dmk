989856e2e84b90450a011b0abf9d2a0a8582876c
robsonferduda <robsonferduda@gmail.com>
2019-05-23 17:40:04 -0300

    Atualizações

--- resources/views/cliente/honorarios.blade.php
+++ resources/views/cliente/honorarios.blade.php
@@ -240,7 +240,7 @@
 
                 $.ajax(
                     {
-                        url: '../cidades-por-estado/'+estado,
+                        url: '/cidades-por-estado/'+estado,
                         type: 'GET',
                         dataType: "JSON",
                         beforeSend: function(){
