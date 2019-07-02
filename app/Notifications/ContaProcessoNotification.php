df7ba4952e7fd4b3861de634b4e2370dd4668608
robsonferduda <robsonferduda@gmail.com>
2019-07-02 18:08:15 -0300

    Resposta de Notificação

---
+++ app/Notifications/ContaProcessoNotification.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ContaProcessoNotification extends Notification
+{
+    use Queueable;
+
+    public $processo;
+
+    public function __construct($processo)
+    {
+        $this->processo = $processo;
+    }
+
+    
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+
+        $decisao = ($notifiable->parecer == 'S') ? 'ACEITOU' : 'RECUSOU' ;
+
+        return (new MailMessage)
+            ->subject(Lang::getFromJson('Resposta de Solicitação'))
+            ->markdown('email.convite')
+            ->line(Lang::getFromJson('A sua solicitação referente ao processo '.$this->processo->nu_processo_pro.' foi respondida por '.$notifiable->correspondente.' e ele '.$decisao.' sua proposta. Clique no botão abaixo para mais informações:'))
+            ->action(Lang::getFromJson('Ver Processo'), url(config('app.url').route('processos.detalhes', ['token' => $notifiable->token], false)))
+            ->line(Lang::getFromJson('Você já pode dar andamento ao processo. Bom trabalho!'));
+    }
+
+}
