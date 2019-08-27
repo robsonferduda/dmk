55acf12b1284893c27676174f860069ec642e1f8
robsonferduda <robsonferduda@gmail.com>
2019-08-27 16:10:22 -0300

    Correção BUG Despesas

--- resources/views/despesas/lancamentos.blade.php
+++ resources/views/despesas/lancamentos.blade.php
@@ -27,28 +27,44 @@
             <div class="well">
                 <form action="{{ url('despesas/buscar') }}" class="form-inline" method="POST" role="search">
                     {{ csrf_field() }}
-                    <div class="row">
+                    <div class="row" style="margin: 0">
 
-                        <section class="col col-md-2">
-                            <label class="label label-black">Data de Vencimento Inicial</label><br />
-                            <input style="width: 100%" class="form-control mascara_data" placeholder="___ /___ /___" type="text" name="dt_vencimento_inicial" value="{{ \Session::get('dt_vencimento_inicial') ? date('d/m/Y', strtotime(\Session::get('dt_vencimento_inicial'))) : old('dt_vencimento_inicial') }}" >
-                            
+                        <section>
+                            <label class="text-primary" style="margin-bottom: 5px;"><i class="fa fa-info-circle"></i> Determine um intervalo de tempo para busca. Caso o período seja igual a um dia, data inicial e final devem ser iguais</label>
                         </section>
 
-                        <section class="col col-md-2">
-                            <label class="label label-black">Data de Vencimento Final</label><br />
-                            <input style="width: 100%" class="form-control mascara_data" placeholder="___ /___ /___" type="text" name="dt_vencimento_final" value="{{ \Session::get('dt_vencimento_final') ? date('d/m/Y', strtotime(\Session::get('dt_vencimento_final'))) : old('dt_vencimento_final') }}" >
-                            
+                        <section class="col col-md-6 box_busca_data">
+                            <section class="col col-md-4">
+                                <h2 class="pull-right">VENCIMENTO</h2>
+                            </section>
+
+                            <section class="col col-md-4">
+                                <label class="label label-black">Data Inicial</label><br />
+                                <input style="width: 100%" class="form-control mascara_data" placeholder="___ /___ /___" type="text" name="dt_vencimento_inicial" value="{{ \Session::get('dt_vencimento_inicial') ? date('d/m/Y', strtotime(\Session::get('dt_vencimento_inicial'))) : old('dt_vencimento_inicial') }}" >
+                                
+                            </section>
+
+                            <section class="col col-md-4">
+                                <label class="label label-black">Data Final</label><br />
+                                <input style="width: 100%" class="form-control mascara_data" placeholder="___ /___ /___" type="text" name="dt_vencimento_final" value="{{ \Session::get('dt_vencimento_final') ? date('d/m/Y', strtotime(\Session::get('dt_vencimento_final'))) : old('dt_vencimento_final') }}" >
+                                
+                            </section>
                         </section>
 
-                        <section class="col col-md-2">                           
-                            <label class="label label-black">Data de Pagamento Inicial</label><br />
-                            <input style="width: 100%" class="form-control mascara_data" placeholder="___ /___ /___" type="text" name="dt_pagamento_inicial" value="{{ \Session::get('dt_pagamento_inicial') ? date('d/m/Y', strtotime(\Session::get('dt_pagamento_inicial'))) : old('dt_pagamento_inicial') }}">                            
-                        </section>
+                        <section class="col col-md-6 box_busca_data">
+                            <section class="col col-md-4">
+                                <h2 class="pull-right">PAGAMENTO</h2>
+                            </section>
 
-                        <section class="col col-md-2">                           
-                            <label class="label label-black">Data de Pagamento Final</label><br />
-                            <input style="width: 100%" class="form-control mascara_data" placeholder="___ /___ /___" type="text" name="dt_pagamento_final" value="{{ \Session::get('dt_pagamento_final') ? date('d/m/Y', strtotime(\Session::get('dt_pagamento_final'))) : old('dt_pagamento_final')  }}">                            
+                            <section class="col col-md-4">                           
+                                <label class="label label-black">Data Inicial</label><br />
+                                <input style="width: 100%" class="form-control mascara_data" placeholder="___ /___ /___" type="text" name="dt_pagamento_inicial" value="{{ \Session::get('dt_pagamento_inicial') ? date('d/m/Y', strtotime(\Session::get('dt_pagamento_inicial'))) : old('dt_pagamento_inicial') }}">                            
+                            </section>
+
+                            <section class="col col-md-4">                           
+                                <label class="label label-black">Data Final</label><br />
+                                <input style="width: 100%" class="form-control mascara_data" placeholder="___ /___ /___" type="text" name="dt_pagamento_final" value="{{ \Session::get('dt_pagamento_final') ? date('d/m/Y', strtotime(\Session::get('dt_pagamento_final'))) : old('dt_pagamento_final')  }}">                            
+                            </section>
                         </section>
 
                     </div>
