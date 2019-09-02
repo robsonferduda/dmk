4305dc12698855ded5a50dde34029bedc9cdcc30
robsonferduda <robsonferduda@gmail.com>
2019-09-02 19:08:45 -0300

    Atualizações

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
     <head>
         <meta charset="utf-8">
-        <title>{{ "DMK" }}</title>
+        <title>{{ env('APP_NAME') }}</title>
         <meta name="description" content="">
         <meta name="author" content="">
         <meta name="token" content="{{ csrf_token() }}">
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt-br" id="extr-page" style="height: 100%;">
 	<head>
 		<meta charset="utf-8">
-		<title>DMK</title>
+		<title>{{ env('APP_NAME') }}</title>
 		<meta name="description" content="">
 		<meta name="author" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
