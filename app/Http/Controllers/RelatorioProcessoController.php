8ba80c1dc2ba4c1fe49cdf75cc6652a119141f7a
rafael01costa <rafael01costa@gmail.com>
2019-09-30 22:17:48 -0300

    Pauta diária

--- app/Http/Controllers/RelatorioProcessoController.php
+++ app/Http/Controllers/RelatorioProcessoController.php
@@ -136,7 +136,7 @@ class RelatorioProcessoController extends Controller
             }
         }
 
-        $processos = $processos->get();
+        $processos = $processos->orderBy('dt_prazo_fatal_pro')->get();
 
         return \Excel::download(new ProcessoPautaDiariaExport(['processos' => $processos]),'Pauta.xlsx',\Maatwebsite\Excel\Excel::XLSX);
     }
