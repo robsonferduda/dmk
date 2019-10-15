203a1da613e3b7cdf41fbae5e0e7cd174edcb15a
rafael01costa <rafael01costa@gmail.com>
2019-10-15 20:17:54 -0300

    Tipo de PRocessos

--- app/Http/Controllers/RelatorioProcessoController.php
+++ app/Http/Controllers/RelatorioProcessoController.php
@@ -105,6 +105,7 @@ class RelatorioProcessoController extends Controller
         $processos = Processo::with('cidade')->where('cd_conta_con',$this->conta)->whereNotIn('cd_status_processo_stp',[\StatusProcesso::FINALIZADO,\StatusProcesso::CANCELADO]);
 
         $responsavel = $request->responsavel;
+        $tipoProcesso = $request->tipoProcesso;
 
         if(!empty($responsavel)){
 
@@ -135,6 +136,10 @@ class RelatorioProcessoController extends Controller
                 }
 
             }
+        }
+
+        if(!empty($tipoProcesso)){
+            $processos = $processos->where('cd_tipo_processo_tpo',$tipoProcesso);
         }
 
         $processos = $processos->orderBy('dt_prazo_fatal_pro')->get();
