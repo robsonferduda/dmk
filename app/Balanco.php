521fe6bb1f8a8d460e247a82cf7c6b257354c22a
rafael01costa <rafael01costa@gmail.com>
2019-10-17 21:22:45 -0300

    Mudando o nome da tabela no model

--- app/Balanco.php
+++ app/Balanco.php
@@ -10,7 +10,7 @@ class Balanco extends Model
 
 	
 
-    protected $table = 'balanco_bal';
+    protected $table = 'vi_balanco_bal';
     protected $primaryKey = "cd_conta_con";
     protected $fillable = [
     						'categoria',
