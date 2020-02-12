bbd6f0016c6c120471ece4df3cab3bbcaea4dd2c
Rafael <rafael@coperve.ufsc.br>
2020-02-12 15:05:37 -0300

    Changes

--- resources/views/exports/balanco-entradas-detalhado.blade.php
+++ resources/views/exports/balanco-entradas-detalhado.blade.php
@@ -19,22 +19,43 @@
     <tbody>
         @php
             $total = 0;
+            $tipo = $dados['tipo'];
         @endphp
       
         @foreach($dados['entradas'] as $dado)
+        @php
+            $totalHonorarioEntrada = 0;
+            if($tipo == 'R'){
+
+                if(!empty($dado->honorario->baixaHonorario))
+                     $totalHonorarioEntrada = $dado->honorario->baixaHonorario->where('cd_tipo_financeiro_tfn',\TipoFinanceiro::ENTRADA)->where('cd_tipo_baixa_honorario_bho', \TipoBaixaHonorario::HONORARIO)->sum('vl_baixa_honorario_bho');
+            }
+        @endphp
         <tr>
             <td> {{ !empty($dado->honorario->dt_baixa_cliente_pth) ? date('d/m/Y', strtotime($dado->honorario->dt_baixa_cliente_pth)) : '' }}</td>
         	<td>{{ $dado->cliente->nm_razao_social_cli }}</td>
             <td>{{ $dado->nu_processo_pro }}</td>   
             <td>{{ date('d/m/Y', strtotime($dado->dt_prazo_fatal_pro)) }}</td>     
             <td>{{ $dado->honorario ? $dado->honorario->tipoServico->nm_tipo_servico_tse : ' ' }}</td>
-            <td>{{ $dado->honorario ? number_format($dado->honorario->vl_taxa_honorario_cliente_pth, 2,',',' ') : number_format(0, 2,',',' ') }}</td>
+
+            @if($tipo == 'P')
+                <td>{{ $dado->honorario ? number_format($dado->honorario->vl_taxa_honorario_cliente_pth, 2,',',' ') : number_format(0, 2,',',' ') }}</td>
+            @else
+                <td>{{ $dado->honorario ? number_format($totalHonorarioEntrada, 2,',',' ') : number_format(0, 2,',',' ') }}</td>
+            @endif
+
+           
              @php
                 $totalDespesas = 0;
-                foreach($dado->tiposDespesa as $despesa){
-                    if($despesa->pivot->cd_tipo_entidade_tpe == \TipoEntidade::CLIENTE && $despesa->pivot->fl_despesa_reembolsavel_pde == 'S'){
-                        $totalDespesas += $despesa->pivot->vl_processo_despesa_pde;
+
+                if($tipo == 'P'){
+                    foreach($dado->tiposDespesa as $despesa){
+                        if($despesa->pivot->cd_tipo_entidade_tpe == \TipoEntidade::CLIENTE && $despesa->pivot->fl_despesa_reembolsavel_pde == 'S'){
+                            $totalDespesas += $despesa->pivot->vl_processo_despesa_pde;
+                        }
                     }
+                }else{
+                    $totalDespesas = $dado->honorario->baixaHonorario->where('cd_tipo_financeiro_tfn',\TipoFinanceiro::ENTRADA)->where('cd_tipo_baixa_honorario_bho', \TipoBaixaHonorario::DESPESA)->sum('vl_baixa_honorario_bho');
                 }
 
              @endphp
@@ -44,8 +65,18 @@
             @php
                 $totalLinha = 0;
                 if(!empty($dado->honorario)){
-                    $totalLinha = (($dado->honorario->vl_taxa_honorario_cliente_pth)-
+
+                    if($tipo == 'P'){
+
+                        $totalLinha = (($dado->honorario->vl_taxa_honorario_cliente_pth)-
                                     ((($dado->honorario->vl_taxa_honorario_cliente_pth)*$dado->honorario->vl_taxa_cliente_pth)/100))+$totalDespesas;
+                    }else{
+
+                        if(!empty($dado->honorario->baixaHonorario)){
+                            $totalLinha = (($totalHonorarioEntrada)-
+                                    ((($totalHonorarioEntrada)*$dado->honorario->vl_taxa_cliente_pth)/100))+$totalDespesas;
+                        }
+                    }
                 }
                 $total +=  $totalLinha;
             @endphp
--- resources/views/exports/balanco-saidas-detalhado.blade.php
+++ resources/views/exports/balanco-saidas-detalhado.blade.php
@@ -18,22 +18,42 @@
     <tbody>
         @php
             $total = 0;
+            $tipo = $dados['tipo'];
         @endphp
       
         @foreach($dados['saidas'] as $dado)
+        @php
+            $totalHonorarioSaida = 0;
+            if($tipo == 'R'){
+
+                if(!empty($dado->honorario->baixaHonorario))
+                     $totalHonorarioSaida = $dado->honorario->baixaHonorario->where('cd_tipo_financeiro_tfn',\TipoFinanceiro::SAIDA)->where('cd_tipo_baixa_honorario_bho', \TipoBaixaHonorario::HONORARIO)->sum('vl_baixa_honorario_bho');
+            }
+        @endphp
         <tr>
             <td> {{ !empty($dado->honorario->dt_baixa_correspondente_pth) ? date('d/m/Y', strtotime($dado->honorario->dt_baixa_correspondente_pth)) : '' }}</td>
         	<td>{{ $dado->correspondente->contaCorrespondente->nm_conta_correspondente_ccr  }}</td>
             <td>{{ $dado->nu_processo_pro }}</td>   
             <td>{{ date('d/m/Y', strtotime($dado->dt_prazo_fatal_pro)) }}</td>     
             <td>{{ !empty($dado->honorario->tipoServicoCorrespondente) ? $dado->honorario->tipoServicoCorrespondente->nm_tipo_servico_tse : ' ' }}</td>
-            <td>{{ $dado->honorario ? number_format($dado->honorario->vl_taxa_honorario_correspondente_pth, 2,',',' ') : number_format(0, 2,',',' ') }}</td>
+
+            @if($tipo == 'P')
+                <td>{{ $dado->honorario ? number_format($dado->honorario->vl_taxa_honorario_correspondente_pth, 2,',',' ') : number_format(0, 2,',',' ') }}</td>
+            @else
+                <td>{{ $dado->honorario ? number_format($totalHonorarioSaida, 2,',',' ') : number_format(0, 2,',',' ') }}</td>
+            @endif
+           
              @php
                 $totalDespesas = 0;
-                foreach($dado->tiposDespesa as $despesa){
-                    if($despesa->pivot->cd_tipo_entidade_tpe == \TipoEntidade::CORRESPONDENTE && $despesa->pivot->fl_despesa_reembolsavel_pde == 'S'){
-                        $totalDespesas += $despesa->pivot->vl_processo_despesa_pde;
+
+                if($tipo == 'P'){
+                    foreach($dado->tiposDespesa as $despesa){
+                        if($despesa->pivot->cd_tipo_entidade_tpe == \TipoEntidade::CORRESPONDENTE && $despesa->pivot->fl_despesa_reembolsavel_pde == 'S'){
+                            $totalDespesas += $despesa->pivot->vl_processo_despesa_pde;
+                        }
                     }
+                }else{
+                    $totalDespesas = $dado->honorario->baixaHonorario->where('cd_tipo_financeiro_tfn',\TipoFinanceiro::SAIDA)->where('cd_tipo_baixa_honorario_bho', \TipoBaixaHonorario::DESPESA)->sum('vl_baixa_honorario_bho');
                 }
 
              @endphp
@@ -42,7 +62,17 @@
             @php
                 $totalLinha = 0;
                 if(!empty($dado->honorario)){
-                    $totalLinha = $dado->honorario->vl_taxa_honorario_correspondente_pth+$totalDespesas;
+
+                    if($tipo == 'P'){
+
+                        $totalLinha = $dado->honorario->vl_taxa_honorario_correspondente_pth+$totalDespesas;
+
+                    }else{
+
+                       $totalLinha = $totalHonorarioSaida+$totalDespesas;
+                    }
+
+                    
                 }
                 $total +=  $totalLinha;
             @endphp
