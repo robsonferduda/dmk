c24ff4de4ee24c7212de883c0ee45e2a08604ccf
Rafael <rafael@coperve.ufsc.br>
2020-03-09 09:39:42 -0300

    Ajuste

--- resources/views/financeiro/entrada.blade.php
+++ resources/views/financeiro/entrada.blade.php
@@ -129,7 +129,7 @@
 
                                 @endphp
 
-                                <tr {{ ($totalBaixaHonorario <= 0 
+                                <tr {{ ($totalBaixaHonorario <= 0 && $totalDespesas+$entrada->vl_taxa_honorario_cliente_pth > 0
                                                     ? 'style=background-color:#fb8e7e' : 
                                                             ($totalBaixaHonorario < ($totalDespesas+$entrada->vl_taxa_honorario_cliente_pth) && $totalBaixaHonorario > 0
                                                                 ? 'style=background-color:#f2cf59' : 
