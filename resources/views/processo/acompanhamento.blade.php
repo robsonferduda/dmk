96af9de0657c090550a4886859c8f9bb1035fb27
robsonferduda <robsonferduda@gmail.com>
2020-04-20 13:27:34 -0300

    Processos - Correção Exclusão BUG

--- resources/views/processo/acompanhamento.blade.php
+++ resources/views/processo/acompanhamento.blade.php
@@ -232,7 +232,7 @@
                                     '<a title="Clonar" class="dialog_clone icone-acompanhamento" href="../processos/clonar/'+data.hash+'"><i class="fa fa-clone"></i> Clonar</a> '+
                                     '<a title="Relatório" class="icone-acompanhamento" href="../processos/relatorio/'+data.hash+'"><i class="fa fa-usd"></i> Relatório Financeiro</a> '+
                                     '<a title="Editar" class="icone-acompanhamento" class="editar_vara" href="../processos/editar/'+data.hash+'"><i class="fa fa-edit"></i> Editar </a> '+
-                                    '<a title="Excluir" data-url="../processos/" class="excluir_registro icone-acompanhamento" href="#"><i class="fa fa-trash"></i> Excluir</a>'+
+                                    '<a title="Excluir" data-id="'+data.cd_processo_pro+'" data-url="../processos/" class="excluir_registro icone-acompanhamento" href="#"><i class="fa fa-trash"></i> Excluir</a>'+
                                 '</div>'+
                                 '<div class="hidden-md hidden-lg col-md-6">'+
                                     '<a title="Despesas" class="icone-acompanhamento" href=""><i class="fa fa-money"></i> </a>'+
@@ -240,7 +240,7 @@
                                     '<a title="Clonar" class="icone-acompanhamento" class="dialog_clone" href=""><i class="fa fa-clone"></i> </a>'+
                                     '<a title="Relatório" class="icone-acompanhamento" href=""><i class="fa fa-usd"></i> </a>'+
                                     '<a title="Editar" class="icone-acompanhamento" class="editar_vara" href=""><i class="fa fa-edit"></i> </a>'+
-                                    '<a title="Excluir" data-url="../processos/" class="excluir_registro icone-acompanhamento" href="#"><i class="fa fa-trash"></i> </a>'+
+                                    '<a title="Excluir" data-id="'+data.cd_processo_pro+'" data-url="../processos/" class="excluir_registro icone-acompanhamento" href="#"><i class="fa fa-trash"></i> </a>'+
                                 '</div>'+
                             '</div>'+
                         '</div>');
@@ -307,7 +307,7 @@
                                     '<a title="Clonar" class="dialog_clone icone-acompanhamento" href="../processos/clonar/'+data.hash+'"><i class="fa fa-clone"></i> Clonar</a> '+
                                     '<a title="Relatório" class="icone-acompanhamento" href="../processos/relatorio/'+data.hash+'"><i class="fa fa-usd"></i> Relatório Financeiro</a> '+
                                     '<a title="Editar" class="icone-acompanhamento" class="editar_vara" href="../processos/editar/'+data.hash+'"><i class="fa fa-edit"></i> Editar </a> '+
-                                    '<a title="Excluir" data-url="../processos/" class="excluir_registro icone-acompanhamento" href="#"><i class="fa fa-trash"></i> Excluir</a>'+
+                                    '<a title="Excluir" data-id="'+data.cd_processo_pro+'" data-url="../processos/" class="excluir_registro icone-acompanhamento" href="#"><i class="fa fa-trash"></i> Excluir</a>'+
                                 '</div>'+
                                 '<div class="hidden-md hidden-lg col-md-6">'+
                                     '<a title="Despesas" class="icone-acompanhamento" href=""><i class="fa fa-money"></i> </a>'+
@@ -315,7 +315,7 @@
                                     '<a title="Clonar" class="icone-acompanhamento" class="dialog_clone" href=""><i class="fa fa-clone"></i> </a>'+
                                     '<a title="Relatório" class="icone-acompanhamento" href=""><i class="fa fa-usd"></i> </a>'+
                                     '<a title="Editar" class="icone-acompanhamento" class="editar_vara" href=""><i class="fa fa-edit"></i> </a>'+
-                                    '<a title="Excluir" data-url="../processos/" class="excluir_registro icone-acompanhamento" href="#"><i class="fa fa-trash"></i> </a>'+
+                                    '<a title="Excluir" data-id="'+data.cd_processo_pro+'" data-url="../processos/" class="excluir_registro icone-acompanhamento" href="#"><i class="fa fa-trash"></i> </a>'+
                                 '</div>'+
                             '</div>'+
                         '</div>');
