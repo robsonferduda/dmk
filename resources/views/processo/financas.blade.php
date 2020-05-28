c3b41b59ae886f1c1472dc673c920d279be538a2
rafael01costa <rafael01costa@gmail.com>
2020-05-28 15:17:30 -0300

    Chamado #10

--- resources/views/processo/financas.blade.php
+++ resources/views/processo/financas.blade.php
@@ -3,8 +3,8 @@
 <div id="ribbon">
     <ol class="breadcrumb">
         <li><a href="{{ url('home') }}">Início</a></li>
-        <li><a href="{{ url('clientes') }}">Clientes</a></li>
-        <li>Honorários por Tipo de Serviço</li>
+        <li><a href="{{ url('clientes') }}">Processos</a></li>
+        <li>Despesas</li>
     </ol>
 </div>
 <div id="content">
@@ -119,78 +119,31 @@
                     </div>
                 </div>
             </article>
-          {{--  <article class="col-sm-12 col-md-12 col-lg-12 sortable-grid ui-sortable">
-                <div class="jarviswidget jarviswidget-sortable">
-                    <header role="heading" class="ui-sortable-handle">
-                        <span class="widget-icon"> <i class="fa fa-money"></i> </span>
-                        <h2>Honorário</h2>             
-                    </header>
-                    <div class="col-sm-12">
-                        <div class="well">
-                            <div class="alert alert-info" role="alert">
-                                <i class="fa-fw fa fa-info"></i>
-                                <strong>Informação!</strong> Os campos de valor serão preenchidos com os valores padrões cadastrados no Cliente e/ou Correspondente ao selecionar o tipo de serviço. Sendo permitida sua mudança.                          
-                            </div>
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <div class="row">
-                                        <div class="col-md-12">   
-                                        
-                                            <div class="col-md-6"> 
-                                                <h5>Honorário por tipo de serviço</h5> 
-                                            </div>
-                                            <div class="col-md-6"> 
-                                                <button class="btn btn-success pull-right header-btn" id="btnSalvarHonorariosProcesso" style="margin-right: -12px;"><i class="fa fa-save fa-lg"></i> Salvar Alterações</button>
-                                               
-                                            </div>                                                                                             
-                                            <div class="tabelah">
-                                                <table class="table table-bordered">
-                                                    <thead>
-                                                        <th style="width: 50%">Tipos de Serviços</th>
-                                                        <th style="text-align: center; ">Valor Cliente</th>
-                                                        <th style="text-align: center; ">Valor Correspondente</th>
-                                                    </thead>
-                                                    <tbody>  
-                                                        <tr>  
-                                                            <td>                                           
-                                                                <select id="tipoServico" name="cd_tipo_servico_tse" class="select2">
-                                                                    <option data-cliente="" data-correspondente="" selected value="">Selecione...</option>  
-                                                                    @foreach($tiposDeServico as $tipoDeServico)
-                                                                        <option {{ (!empty($honorariosProcesso) && $honorariosProcesso->cd_tipo_servico_tse ==  $tipoDeServico->cd_tipo_servico_tse) ? 'selected' : '' }} data-cliente="{{ $tipoDeServico->nu_taxa_the_cliente }}" data-correspondente="{{ $tipoDeServico->nu_taxa_the_correspondente }}" value="{{$tipoDeServico->cd_tipo_servico_tse}}">{{$tipoDeServico->nm_tipo_servico_tse}}</option>  
-                                                                    @endforeach
-                                                                </select>
-                                                            </td>
-                                                            <td>
-                                                                <div class="col-md-4 col-md-offset-2">
-                                                                <div class="input-group">
-                                                                    <span class="input-group-addon">$</span>
-                                                                    <input style="width: 100px;" id="taxa-honorario-cliente" type="text" class="form-control taxa-honorario" value="{{ ( !empty($honorariosProcesso->vl_taxa_honorario_cliente_pth)) ? $honorariosProcesso->vl_taxa_honorario_cliente_pth : '' }}">
-                                                                </div>
-                                                                </div>
-                                                            </td>
-                                                            <td>
-                                                                <div class="col-md-4 col-md-offset-2">
-                                                                <div class="input-group">
-                                                                    <span class="input-group-addon">$</span>
-                                                                    <input style="width: 100px;" id="taxa-honorario-correspondente" type="text" class="form-control taxa-honorario"  value="{{ ( !empty($honorariosProcesso->vl_taxa_honorario_correspondente_pth)) ? $honorariosProcesso->vl_taxa_honorario_correspondente_pth : '' }}">
-                                                                </div>
-                                                                </div>
-                                                            </td>
-                                                            
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </article> --}}
+         
 
         </div>
     </div>
 </div>
+@endsection
+@section('script')
+<script type="text/javascript">
+    $(".taxa-despesa").focus(function(){
+       $("#"+$(this).data("identificador")).prop('checked',true);
+       console.log('checked');
+    });
+
+    $(".taxa-despesa").focusout(function(){
+        if($(this).val().trim() == ''){
+            $("#"+$(this).data("identificador")).prop('checked',false);
+        }
+    });
+
+    $('input[type="checkbox"]').click(function(){
+        
+        if($(this).prop("checked") == false){
+            $("input[data-identificador='"+$(this).attr('id')+"']").val("");
+        }
+    });
+
+</script>
 @endsection
