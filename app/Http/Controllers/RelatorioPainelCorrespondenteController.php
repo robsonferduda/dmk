769ae7514a879cb3c7108d005cc1f57478592860
rafael01costa <rafael01costa@gmail.com>
2020-11-04 16:18:29 -0300

    Relatórios Painel correspondente

--- app/Http/Controllers/RelatorioPainelCorrespondenteController.php
+++ app/Http/Controllers/RelatorioPainelCorrespondenteController.php
@@ -20,100 +20,92 @@ class RelatorioPainelCorrespondenteController extends Controller
         $this->conta = \Session::get('SESSION_CD_CONTA');
     }
 
-    public function index(){   
+    public function index()
+    {
+        //dd(\Session::all());
 
-        return view('correspondente/painel/index',['arquivos' => $this->getFiles()]);
-
+        return view('correspondente/painel/index', ['arquivos' => $this->getFiles()]);
     }
 
-    public function buscar(Request $request){
-        
-        if(\Helper::validaData($request->dtInicio) && \Helper::validaData($request->dtFim) && strtotime(str_replace('/','-',$request->dtInicio)) <= strtotime(str_replace('/','-',$request->dtFim))){
-
-            $dtInicio = date('Y-m-d', strtotime(str_replace('/','-',$request->dtInicio)));
-            $dtFim    = date('Y-m-d', strtotime(str_replace('/','-',$request->dtFim)));
+    public function buscar(Request $request)
+    {
+        if (\Helper::validaData($request->dtInicio) && \Helper::validaData($request->dtFim) && strtotime(str_replace('/', '-', $request->dtInicio)) <= strtotime(str_replace('/', '-', $request->dtFim))) {
+            $dtInicio = date('Y-m-d', strtotime(str_replace('/', '-', $request->dtInicio)));
+            $dtFim    = date('Y-m-d', strtotime(str_replace('/', '-', $request->dtFim)));
 
             $cliente = $request->cd_conta_con;
 
-            $processos = Processo::where('cd_correspondente_cor', $this->conta) 
-                                    ->whereHas('status', function($query){
-                                        $query->where('fl_visivel_correspondente_stp','S');
+            $processos = Processo::where('cd_correspondente_cor', $this->conta)
+                                    ->whereHas('status', function ($query) {
+                                        $query->where('fl_visivel_correspondente_stp', 'S');
                                     })
                                     ->when(!empty($request->cd_conta_con), function ($query) use ($cliente) {
-                                        return $query->where('cd_conta_con',$cliente);
+                                        return $query->where('cd_conta_con', $cliente);
                                     })
-                                    ->when(!empty($request->finalizado), function ($query){
-                                        return $query->where('cd_status_processo_stp',\StatusProcesso::FINALIZADO);
+                                    ->when(!empty($request->finalizado), function ($query) {
+                                        return $query->where('cd_status_processo_stp', \StatusProcesso::FINALIZADO);
                                     })
-                                    ->whereBetween('dt_prazo_fatal_pro',[$dtInicio,$dtFim])
-                                    ->orderBy('dt_prazo_fatal_pro','asc')
+                                    ->whereBetween('dt_prazo_fatal_pro', [$dtInicio,$dtFim])
+                                    ->orderBy('dt_prazo_fatal_pro', 'asc')
                                     ->orderBy('hr_audiencia_pro')
                                     ->get();
 
-            if(!empty($cliente)){
-
-                $conta = Conta::where('cd_conta_con',$cliente)->first();
+            if (!empty($cliente)) {
+                $conta = Conta::where('cd_conta_con', $cliente)->first();
                 $fileName = '_Relação Processos_'.$conta->nm_razao_social_con;
-            }else{
+            } else {
                 $fileName = '_Relação Processos_Todos';
                 $conta = '';
             }
 
-            $dados = array('processos' => $processos, 'cliente' => $conta);    
+            $dados = array('processos' => $processos, 'cliente' => $conta);
 
-            if(!$processos->isEmpty()){
-                \Excel::store(new RelacaoProcessosExport($dados),"/correspondente/{$this->conta}/".time().$fileName.'.xlsx','reports',\Maatwebsite\Excel\Excel::XLSX);
-            }else{
+            if (!$processos->isEmpty()) {
+                \Excel::store(new RelacaoProcessosExport($dados), "/correspondentes/{$this->conta}/reports/".time().$fileName.'.xlsx', 'arquivos', \Maatwebsite\Excel\Excel::XLSX);
+            } else {
                 Flash::error('Não há dados para os parâmetros informados!');
-
             }
-        
-        }else{
-
+        } else {
             Flash::error('Data(s) inválida(s) !');
-
         }
 
-        return \Redirect::back()->with('dtInicio',str_replace('/','',$request->dtInicio))
-                                ->with('dtFim' ,str_replace('/','',$request->dtFim))
-                                ->with('relatorio',$request->relatorio)                                
-                                ->with('finalizado',$request->finalizado)
+        return \Redirect::back()->with('dtInicio', str_replace('/', '', $request->dtInicio))
+                                ->with('dtFim', str_replace('/', '', $request->dtFim))
+                                ->with('relatorio', $request->relatorio)
+                                ->with('finalizado', $request->finalizado)
 
-                                ->with('conta',$request->cd_conta_con)
-                                ->with('nmConta',$request->nm_conta_con); 
+                                ->with('conta', $request->cd_conta_con)
+                                ->with('nmConta', $request->nm_conta_con);
     }
 
-    private function getFiles(){
-
-        \File::makeDirectory(storage_path().'/reports/correspondente/'.$this->conta, $mode = 0777, true, true);
+    private function getFiles()
+    {
+        \File::makeDirectory(storage_path()."/arquivos/correspondentes/{$this->conta}/reports/", $mode = 0777, true, true);
 
         $arquivos = array();
 
-        $files = collect(\File::allFiles(storage_path()."/reports/correspondente/".$this->conta))
+        $files = collect(\File::allFiles(storage_path()."/arquivos/correspondentes/{$this->conta}/reports/"))
                  ->sortByDesc(function ($file) {
-                    return $file->getCTime();
-                });
+                     return $file->getCTime();
+                 });
         
-        foreach($files as $file){
-            
-            $arquivos[] = array('nome' => $file->getFilename(), 'data' => date('d/m/Y H:i:s',$file->getCTime()),'tamanho' => round($file->getSize()/1024,2) );           
+        foreach ($files as $file) {
+            $arquivos[] = array('nome' => $file->getFilename(), 'data' => date('d/m/Y H:i:s', $file->getCTime()),'tamanho' => round($file->getSize()/1024, 2) );
         }
 
         return $arquivos;
     }
 
-    public function excluir($nome){
-    
-       \Storage::disk('reports')->delete("/correspondente/$this->conta/".$nome);
+    public function excluir($nome)
+    {
+        \Storage::disk('arquivos')->delete("/correspondentes/{$this->conta}/reports/".$nome);
         
-        return \Response::json(array('message' => 'Registro excluído com sucesso'), 200);    
-
+        return \Response::json(array('message' => 'Registro excluído com sucesso'), 200);
     }
 
-    public function arquivo($nome){
+    public function arquivo($nome)
+    {
         //dd(\Storage::disk('reports')->get("$this->conta/".$nome));
-        return response()->download(storage_path('reports/correspondente/'."$this->conta/".$nome));
-
+        return response()->download(storage_path("/arquivos/correspondentes/{$this->conta}/reports/".$nome));
     }
-
 }
