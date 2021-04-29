db7c05eb47fd178b63c3816a20dd048c29d49d17
rafael01costa <rafael01costa@gmail.com>
2021-04-29 17:40:31 -0300

    Ajuste na tela de cidade

--- app/Http/Controllers/CidadeController.php
+++ app/Http/Controllers/CidadeController.php
@@ -14,13 +14,10 @@ use Laracasts\Flash\Flash;
 
 class CidadeController extends Controller
 {
-
-
     public function __construct()
     {
         $this->middleware('auth');
         $this->conta = \Session::get('SESSION_CD_CONTA');
-        
     }
 
     public function buscaCidadePorEstado($estados)
@@ -28,33 +25,29 @@ class CidadeController extends Controller
         $cidades = array();
 
         if (empty(\Cache::tags(['estados', $estados])->get('cidades'))) {
-            
-            $cidades = Cidade::select('cd_cidade_cde','nm_cidade_cde','cd_estado_est')->whereIn('cd_estado_est', explode(",", $estados))->orderBy('nm_cidade_cde')->with(['estado' => function($query) {
+            $cidades = Cidade::select('cd_cidade_cde', 'nm_cidade_cde', 'cd_estado_est')->whereIn('cd_estado_est', explode(",", $estados))->orderBy('nm_cidade_cde')->with(['estado' => function ($query) {
                 $query->select('cd_estado_est', 'sg_estado_est');
-            }])->orderBy('nm_cidade_cde')->get(); 
+            }])->orderBy('nm_cidade_cde')->get();
 
-            \Cache::tags(['estados', $estados])->put('cidades', $cidades,now()->addMinutes(1440));
-                              
-        }else{
+            \Cache::tags(['estados', $estados])->put('cidades', $cidades, now()->addMinutes(1440));
+        } else {
             $cidades = \Cache::tags(['estados', $estados])->get('cidades');
-        }
-
+        }       
         echo json_encode($cidades);
     }
 
-    public function buscaCidadePorEstadoCorrespondente($entidade,$estado)
+    public function buscaCidadePorEstadoCorrespondente($entidade, $estado)
     {
         $cidades = array();
 
         $cidades = DB::table('cidade_atuacao_cat')
-                    ->select('cidade_cde.cd_cidade_cde','nm_cidade_cde')
+                    ->select('cidade_cde.cd_cidade_cde', 'nm_cidade_cde')
                     ->join('cidade_cde', 'cidade_atuacao_cat.cd_cidade_cde', '=', 'cidade_cde.cd_cidade_cde')
                     ->join('estado_est', 'estado_est.cd_estado_est', '=', 'cidade_cde.cd_estado_est')
-                    ->where('cd_entidade_ete',$entidade)
-                    ->where('estado_est.cd_estado_est',$estado)
+                    ->where('cd_entidade_ete', $entidade)
+                    ->where('estado_est.cd_estado_est', $estado)
                     ->get();
                              
         echo json_encode($cidades);
     }
-
 }
