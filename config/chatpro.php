0b68b2889d07ad8e96575ecb28a9e06a93aa3dd7
Robson Fernando Duda <robsonferduda@gmail.com>
2026-05-17 14:13:55 -0300

    Log - Chat

--- config/chatpro.php
+++ config/chatpro.php
@@ -14,17 +14,7 @@
 */
 
 return [
-    // Lembrete PRÉ-diligência enviado ao correspondente um dia antes da audiência.
-    // Placeholders: {correspondente}, {processo}, {reu}, {vara}, {cidade}, {data}, {hora_audiencia}
-    'lembrete_prediligencia' =>
-        "Olá, tudo bem?\n\n" .
-        "Gostaríamos de confirmar se está tudo certo para a realização da(s) audiência(s). Todos os documentos e orientações serão anexados em nosso sistema.\n\n" .
-        "Pedimos que assim que receber os documentos e orientações, confirmem o recebimento via sistema AINDA HOJE apertando o botão que fica abaixo dos documentos CONFIRMA O RECEBIMENTO DOS DOCUMENTOS E A REALIZAÇÃO DO ATO, para que possamos ter a gestão completa da pauta de audiências.\n\n" .
-        "Caso ainda não tenha recebido as orientações é porque ainda não recebemos de nossos clientes e assim que recebermos encaminharemos.\n\n" .
-        "Reforçamos a importância com todos os correspondentes que é obrigatório a confirmações das documentações e orientações no sistema e confirmação da presença no link abaixo.\n\n" .
-        "{link_confirmacao_audiencia}\n\n" .
-        "Para dúvidas, use o WhatsApp ou, em caso de urgência não aguarde LIGUE para 48 991308024. Obrigada!",
-
+    
     // URL base da API ChatPro. A instância entra como segmento na chamada.
     // Ex.: https://v5.chatpro.com.br/{instance_id}/api/v1/send_message
     'base_url' => env('CHATPRO_BASE_URL', 'https://v5.chatpro.com.br'),
@@ -82,6 +72,18 @@ return [
             "📲 Ao chegar no local, realize o check-in no link abaixo:\n" .
             "{link_checkin}\n\n" .
             "Urgências, ligue: (48) 99130-8024",
+
+
+        // Lembrete PRÉ-diligência enviado ao correspondente um dia antes da audiência.
+        // Placeholders: {correspondente}, {processo}, {reu}, {vara}, {cidade}, {data}, {hora_audiencia}
+        'lembrete_prediligencia' =>
+            "Olá, tudo bem?\n\n" .
+            "Gostaríamos de confirmar se está tudo certo para a realização da(s) audiência(s). Todos os documentos e orientações serão anexados em nosso sistema.\n\n" .
+            "Pedimos que assim que receber os documentos e orientações, confirmem o recebimento via sistema AINDA HOJE apertando o botão que fica abaixo dos documentos CONFIRMA O RECEBIMENTO DOS DOCUMENTOS E A REALIZAÇÃO DO ATO, para que possamos ter a gestão completa da pauta de audiências.\n\n" .
+            "Caso ainda não tenha recebido as orientações é porque ainda não recebemos de nossos clientes e assim que recebermos encaminharemos.\n\n" .
+            "Reforçamos a importância com todos os correspondentes que é obrigatório a confirmações das documentações e orientações no sistema e confirmação da presença no link do check in.\n\n" .
+            "Confirme sua audiência aqui: {link_confirmacao_audiencia}\n" .
+            "Para dúvidas, use o WhatsApp ou, em caso de urgência não aguarde LIGUE para 48 991308024. Obrigada!",
 
     ],
 
